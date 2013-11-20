8ccd76612f173e6dd93a9d033f6dc979a8fd8fad
dmaulik <dmaulik93@gmail.com>
2013-11-20 08:38:37 -0800

    adding response recording

---
+++ Maven_DataVisualization/src/main/java/edu/isi/serverbackend/Servlet/AnswerStoreServlet.java
@@ -0,0 +1,65 @@
+package edu.isi.serverbackend.Servlet;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.isi.serverbackend.linkedData.LinkedDataNode;
+
+import org.json.JSONException;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.http.HTTPRepository;
+
+import edu.isi.serverbackend.request.*;
+
+/**
+ * Servlet implementation class LinkRankingServlet
+ */
+@WebServlet("/AnswerStoreServlet")
+public class AnswerStoreServlet extends HttpServlet {
+	//private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AnswerStoreServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Recieved POST request");
+        String a = request.getParameter("A");
+        String b = request.getParameter("B");
+        String interesting = request.getParameter("Interesting");
+
+        File data = new File("answers.txt");
+        if (!data.exists()){
+           data.createNewFile();
+        }
+        PrintWriter writeData = new PrintWriter(new FileWriter(data, true));
+        writeData.println(a + "," + b + "," + interesting);
+        writeData.flush();
+
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+}
