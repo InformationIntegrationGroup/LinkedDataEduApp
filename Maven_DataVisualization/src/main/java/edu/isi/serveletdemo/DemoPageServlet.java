50ae8b2d4a88aeef438853f46d03f77f2f360849
Yuting Liu <liu358@usc.edu>
2013-11-25 08:35:51 -0800

    a file for demoPage

---
+++ Maven_DataVisualization/src/main/java/edu/isi/serveletdemo/DemoPageServlet.java
@@ -0,0 +1,41 @@
+package edu.isi.serveletdemo;
+import java.util.List;
+import org.openrdf.OpenRDFException;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.QueryLanguage;
+
+public class DemoPageServlet {
+	try {
+   RepositoryConnection con = repo.getConnection();
+   try {
+	  String queryString = "SELECT ?x ?y WHERE { ?x ?p ?y } ";
+	  TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+
+	  TupleQueryResult result = tupleQuery.evaluate();
+	  try {
+			BindingSet bindingSet = result.next();
+			Value valueOfX = bindingSet.getValue("x");
+			Value valueOfY = bindingSet.getValue("y");
+
+			// do something interesting with the values here...
+	  }
+	  finaly {
+	      result.close();
+	  }
+   }
+   finally {
+      con.close();
+   }
+}
+catch (OpenRDFException e) {
+   // handle exception
+}
+
+
+
+}
+
+}
