91bc48c157376a869bbf11056225620caae78816
Vincent <jianliac@usc.edu>
2014-02-11 21:17:01 -0800

    add caching code

--- Maven_DataVisualization/src/main/java/edu/isi/serverbackend/request/LinkedDataCachingRequest.java
+++ Maven_DataVisualization/src/main/java/edu/isi/serverbackend/request/LinkedDataCachingRequest.java
@@ -1,116 +1,190 @@
 package edu.isi.serverbackend.request;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.net.UnknownHostException;
+
+import com.mongodb.*;
+
 import java.util.*;
 
-import edu.isi.serverbackend.linkedData.LinkedDataConnection;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.repository.RepositoryException;
+
+import edu.isi.serverbackend.linkedData.*;
 import edu.isi.serverbackend.localDatabase.bean.*;
-import edu.isi.serverbackend.localDatabase.table.ConnectionTable;
-import edu.isi.serverbackend.localDatabase.table.NodeTable;
+import edu.isi.serverbackend.localDatabase.mongoCollection.*;
+import edu.isi.serverbackend.feature.RarityDegree;
+import edu.isi.serverbackend.feature.util.*;
 
 public class LinkedDataCachingRequest{
-	private final String USER = "linkedDataAdmin";
-	private final String PASSWORD = "linkedData";
-	private final String CONN_STRING = "jdbc:mysql://localhost/linked_data_project";
+	private static final String USER = "linkedDataAdmin";
+	private static final String PASSWORD = "";
+	private static final String HOST = "localhost";
+	private static final String DATABASE = "LinkedData";
+	private static final String EXPLORED = "FullyExploredNode";
+	private static final String ADDED = "QueueAddedNode";
+
 	
-	Connection SQLConnection;
+	private DB dbConn;
+	public enum TripleType {Person, Place, Organisation, Work, Event, All};
 	
-	List<LinkedDataConnection> subjectConnections;
-	List<LinkedDataConnection> objectConnections;
 	private CachingThread cachingThread;
 
 	
-	public LinkedDataCachingRequest(Connection sql, List<LinkedDataConnection> subjectConnections, List<LinkedDataConnection> objectConnections){
-		this.subjectConnections = subjectConnections;
-		this.objectConnections = objectConnections;
-		cachingThread = new CachingThread();
-		SQLConnection = sql;
+	public LinkedDataCachingRequest(){
+		try {
+			MongoClient mongo = new MongoClient(HOST);
+			this.dbConn = mongo.getDB(DATABASE);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
-	public void cachNodes(){
-		NodeBean nodeBean = new NodeBean();
-		nodeBean.setNodeName(subjectConnections.get(0).getSubject().getName());
-		nodeBean.setUri(subjectConnections.get(0).getSubject().getURI());
-		nodeBean.setExplored(true);
-		int index = NodeTable.findNode(subjectConnections.get(0).getSubject().getURI(), SQLConnection);
-		if( index != -1){
-			NodeTable.updateNode(index, nodeBean, SQLConnection);
+	public void cachingTripleByBFS(LinkedDataNode seed, TripleType cachingType, long max){
+		String type =  null;
+		List<Sample> samples = new ArrayList<Sample>();
+		Queue<LinkedDataNode> nodeQueue = new LinkedList<LinkedDataNode>();
+		long total = 0;
+		if(cachingType == TripleType.Person){
+			type = "http://dbpedia.org/ontology/Person";
 		}
-		else{
-			NodeTable.insertNode(nodeBean, SQLConnection);
+		else if(cachingType == TripleType.Place){
+			type = "http://dbpedia.org/ontology/Place";
+		}
+		else if(cachingType == TripleType.Organisation){
+			type = "http://dbpedia.org/ontology/Organisation";
+		}
+		else if(cachingType == TripleType.Work){
+			type = "http://dbpedia.org/ontology/Work";
+		}
+		else if(cachingType == TripleType.Event){
+			type = "http://dbpedia.org/ontology/Event";
 		}
 		
-		
-		for(int i = 0; i<subjectConnections.size();i++){
-			nodeBean = new NodeBean();
-			nodeBean.setNodeName(subjectConnections.get(i).getObject().getName());
-			nodeBean.setUri(subjectConnections.get(i).getObject().getURI());
-			nodeBean.setExplored(false);
-			index = NodeTable.findNode(subjectConnections.get(i).getObject().getURI(), SQLConnection);
-			if(index == -1){
-				NodeTable.insertNode(nodeBean, SQLConnection);
+		try {
+			insertNodeIntoTemp(seed, ADDED);
+			seed.retrieveNameAndType();
+			seed.retrieveSubjectConnections(samples);
+			seed.retrieveObjectConnections(samples);
+			RarityDegree.calcuateNodeDegree(samples);
+			RarityDegree.calculateExtensionRarity(samples);
+			total += pushNodeIntoQueueAndStoreTriple(samples, nodeQueue, type);
+			while (!nodeQueue.isEmpty() && total < max) {
+				samples = new ArrayList<Sample>();
+				LinkedDataNode currentNode = nodeQueue.remove();
+				currentNode.retrieveSubjectConnections(samples);
+				currentNode.retrieveObjectConnections(samples);
+				RarityDegree.calcuateNodeDegree(samples);
+				RarityDegree.calculateExtensionRarity(samples);
+				total += pushNodeIntoQueueAndStoreTriple(samples, nodeQueue, type);
+			}
+		} catch (RepositoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedQueryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (QueryEvaluationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private long pushNodeIntoQueueAndStoreTriple(List<Sample> samples, Queue<LinkedDataNode> nodeQueue, String type){
+		DBCollection exploredNodeCollection = dbConn.getCollection(EXPLORED);
+		DBCollection addedNodeCollection = dbConn.getCollection(ADDED);
+		NodeBean currentNodeBean = new NodeBean();
+		long tripleNum = 0;
+		if(!samples.isEmpty()){
+			if(samples.get(0).getLink().isSubjectConnection()){
+				insertNodeIntoTemp(samples.get(0).getLink().getSubject(), EXPLORED);
+				currentNodeBean.setName(samples.get(0).getLink().getSubject().getName());
+				currentNodeBean.setExplored(true);
+				currentNodeBean.setUri(samples.get(0).getLink().getSubject().getURI());
+				currentNodeBean.setTypeURI(samples.get(0).getLink().getSubject().getTypeURI());
+			}
+			else{
+				insertNodeIntoTemp(samples.get(0).getLink().getObject(), EXPLORED);
+				currentNodeBean.setName(samples.get(0).getLink().getObject().getName());
+				currentNodeBean.setExplored(true);
+				currentNodeBean.setUri(samples.get(0).getLink().getObject().getURI());
+				currentNodeBean.setTypeURI(samples.get(0).getLink().getObject().getTypeURI());
 			}
 		}
-		
-		for(int i = 0; i<objectConnections.size();i++){
-			nodeBean = new NodeBean();
-			nodeBean.setNodeName(objectConnections.get(i).getSubject().getName());
-			nodeBean.setUri(objectConnections.get(i).getSubject().getURI());
-			nodeBean.setExplored(false);
-			index = NodeTable.findNode(objectConnections.get(i).getSubject().getURI(), SQLConnection);
-			if(index == -1){
-				NodeTable.insertNode(nodeBean, SQLConnection);
+		for(Sample sample:samples){
+			if(sample.getLink().isSubjectConnection()){
+				BasicDBObject query = new BasicDBObject("uri", sample.getLink().getObject().getURI());
+				DBCursor cursor = exploredNodeCollection.find(query);
+				if(!cursor.hasNext()){
+					cursor = addedNodeCollection.find(query);
+					if(!cursor.hasNext() && (sample.getLink().getObject().getTypeURI().equals(type) || type == null)){
+						nodeQueue.add(sample.getLink().getObject());
+						addedNodeCollection.insert(query);
+					}
+					
+					TripleBean tripleBean =  new TripleBean();
+					PredicateBean predicateBean = new PredicateBean();
+					predicateBean.setURI(sample.getLink().getPredicate());
+					tripleBean.setPredicate(predicateBean);
+					NodeBean node = new NodeBean();
+					node.setExplored(false);
+					node.setName(sample.getLink().getObject().getName());
+					node.setTypeURI(sample.getLink().getObject().getTypeURI());
+					node.setUri(sample.getLink().getObject().getURI());
+					tripleBean.setSubject(currentNodeBean);
+					tripleBean.setObject(node);
+					
+					tripleBean.setSbjExtensionRarity(sample.getSubjectExtensionRarity());
+					tripleBean.setObjExtensionRarity(sample.getObjectExtensionRarity());
+					tripleBean.setSubjectDegree(sample.getSubjectRarity());
+					tripleBean.setObjectDegree(sample.getObjectRarity());
+					TripleCollection.insertTriple(tripleBean, dbConn);
+					tripleNum++;
+				}
+			}
+			else{
+				BasicDBObject query = new BasicDBObject("uri", sample.getLink().getSubject().getURI());
+				DBCursor cursor = exploredNodeCollection.find(query);
+				if(!cursor.hasNext()){
+					cursor = addedNodeCollection.find(query);
+					if(!cursor.hasNext() && (sample.getLink().getSubject().getTypeURI().equals(type) || type == null)){
+						nodeQueue.add(sample.getLink().getSubject());
+						addedNodeCollection.insert(query);
+					}
+					TripleBean tripleBean =  new TripleBean();
+					PredicateBean predicateBean = new PredicateBean();
+					predicateBean.setURI(sample.getLink().getPredicate());
+					tripleBean.setPredicate(predicateBean);
+					NodeBean node = new NodeBean();
+					node.setExplored(false);
+					node.setName(sample.getLink().getSubject().getName());
+					node.setTypeURI(sample.getLink().getSubject().getTypeURI());
+					node.setUri(sample.getLink().getSubject().getURI());
+					tripleBean.setObject(currentNodeBean);
+					tripleBean.setSubject(node);
+					
+					tripleBean.setSbjExtensionRarity(sample.getSubjectExtensionRarity());
+					tripleBean.setObjExtensionRarity(sample.getObjectExtensionRarity());
+					tripleBean.setSubjectDegree(sample.getSubjectRarity());
+					tripleBean.setObjectDegree(sample.getObjectRarity());
+					TripleCollection.insertTriple(tripleBean, dbConn);
+					tripleNum++;
+				}
 			}
 		}
+		return tripleNum;
 	}
 	
-	public void cachSubjectConnections(){
-		int subjectIndex = NodeTable.findNode(subjectConnections.get(0).getSubject().getURI(), SQLConnection);
-		for(int i=0; i<subjectConnections.size(); i++){
-			ConnectionBean cBean = new ConnectionBean();
-			cBean.setRelation(obtainRelation(subjectConnections.get(i).getPredicate()));
-			cBean.setNamespace(obtainNamespace(subjectConnections.get(i).getPredicate()));
-			cBean.setSubjectId(subjectIndex);
-			cBean.setObjectId(NodeTable.findNode(subjectConnections.get(i).getObject().getURI(), SQLConnection));
-			cBean.setObjectRank((int)subjectConnections.get(i).getConnectionParam());
-			ConnectionTable.insertConnection(cBean, SQLConnection);
-		}
-	}
-	
-	public void cachObjectConnections(){
-		int objectIndex = NodeTable.findNode(objectConnections.get(0).getObject().getURI(), SQLConnection);
-		for(int i=0; i<objectConnections.size(); i++){
-			ConnectionBean cBean = new ConnectionBean();
-			cBean.setRelation(obtainRelation(objectConnections.get(i).getPredicate()));
-			cBean.setNamespace(obtainNamespace(objectConnections.get(i).getPredicate()));
-			cBean.setSubjectId(NodeTable.findNode(objectConnections.get(i).getSubject().getURI(), SQLConnection));
-			cBean.setObjectId(objectIndex);
-			cBean.setSubjectRank((int)objectConnections.get(i).getConnectionParam());
-			ConnectionTable.insertConnection(cBean, SQLConnection);
-		}
+	private void insertNodeIntoTemp(LinkedDataNode node, String collectionName){
+		BasicDBObject query = new BasicDBObject("uri", node.getURI());
+		DBCollection tempCollection = dbConn.getCollection(collectionName);
+		tempCollection.insert(query);
 	}
 	
 	public void startCaching(){
 		cachingThread.start();
-	}
-	
-	private String obtainNamespace(String predicate){
-		String[] temp = predicate.split("//");
-		String[] temp2 = temp[1].split("/");
-		String result = "http://";
-		for(int i =0; i<temp2.length-1; i++){
-			result = result +temp2[i]+"/";
-		}
-		
-		return result;
-	}
-	
-	private String obtainRelation(String predicate){
-		String[] temp = predicate.split("//");
-		String[] temp2 = temp[1].split("/");
-		return temp2[temp2.length-1];
 	}
 	
 	private class CachingThread extends Thread{
@@ -120,13 +194,8 @@ public class LinkedDataCachingRequest{
 		}
 		
 		public void run(){
-			System.out.println("Caching thread starts.");
-			cachNodes();
-			cachSubjectConnections();
-			cachObjectConnections();
-			System.out.println("Caching thread ends.");
+			
 		}
 	}
-	
 	
 }
